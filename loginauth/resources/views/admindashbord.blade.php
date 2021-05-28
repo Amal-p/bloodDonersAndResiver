089e4a420505fe6f7ca4e3681458a961b7c2532b
Amal-p <amalpvatayam67@gmail.com>
2021-05-29 00:23:16 +0530

    Asset change to secure_asset

--- loginauth/resources/views/admindashbord.blade.php
+++ loginauth/resources/views/admindashbord.blade.php
@@ -7,7 +7,7 @@
      <!--Bootstrap css-->
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!--css on public folder-->
-    <link rel="stylesheet" href="{{asset('css/admindashbord.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/admindashbord.css')}}">
     <!--fontawesome-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Admin - Dashbord</title>
